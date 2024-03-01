88e362be6efc339eb36370c2f8a39b056c92a33e
Faranaz08 <83759275+Faranaz08@users.noreply.github.com>
2024-03-01 20:20:37 +0530

    Update database.js

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,4 +1,8 @@
 const Sequelize = require('sequelize');
+const dotenv=require("dotenv");
+
+//get config vars
+dotenv.config();
 
 const sequelize = new Sequelize('expenstracker', 'root', 'faranaz', {dialect: 'mysql', host:'localhost'});
 
